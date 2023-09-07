38d64583716e78e4ed70e2e01313ca097f578f16
Nikolay Núñez <nikolaynunezballadares@gmail.com>
2023-09-07 22:13:12 +0200

    -

--- src/App.js
+++ src/App.js
@@ -2,19 +2,64 @@ import { useState } from 'react';
 import './App.css';
 import Header from "./components/Header/Header"
 import Formulario from './components/Formulario/Formulario'
-import MiOrg from './components/MiOrg/MiOrg';
+import MiOrg from './components/MiOrg/MiOrg'
+import Equipo from './components/Equipo/Equipo';
 
 function App() {
 
-  const [mostrarFormulario, actualizarMostrar] = useState(true);
+  const [mostrarFormulario, actualizarMostrar] = useState(false);
 
   const cambiarMostrar = () => {actualizarMostrar(!mostrarFormulario)};
+
+  const equipos = [
+    {
+      titulo: "Programación",
+      colorPrimario: "#57C278",
+      colorSecundario: "#D9F7E9"
+    },
+    {
+      titulo: "Front-End",
+      colorPrimario: "#82CFFA",
+      colorSecundario: "#E8F8FF"
+    },  
+    {
+      titulo: "Data Science",
+      colorPrimario: "#A6D157",
+      colorSecundario: "#F0F8E2"
+    },  
+    {
+      titulo: "Devops",
+      colorPrimario: "#E06B69",
+      colorSecundario: "#FDE7E8"
+    },  
+    {
+      titulo: "UX y Diseño",
+      colorPrimario: "#DB6EBF",
+      colorSecundario: "#FAE9F5"
+    },  
+    {
+      titulo: "Móvil",
+      colorPrimario: "#FFBA05",
+      colorSecundario: "#FFF5D9"
+    },  
+    {
+      titulo: "Innovación y Gestión",
+      colorPrimario: "#FF8A29",
+      colorSecundario: "#FFEEDF"
+    }
+  ]
 
   return (
     <div className="App">
       <Header />
       {mostrarFormulario === true ? <Formulario /> : <></>}
       <MiOrg cambiarMostrar={cambiarMostrar}/>
+      {
+        equipos.map((equipo) => {
+          console.log("Equipo", equipo)
+          return <Equipo datos={equipo} />
+        })
+      }
     </div>
   );
 }
